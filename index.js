2cc1d9afd250f5f25a206fdcc1468eed504b66b2
Brian Westphal <typescript.oss@gmail.com>
2025-02-26 08:24:21 +0800

    - Added support for configuring expected extensions, ex. to allow mjs, cjs, jsx, ts, tsx, etc. - Fixed issue with using in eslint flat config (https://github.com/anza-xyz/eslint-plugin-require-extensions/issues/13) (https://github.com/anza-xyz/eslint-plugin-require-extensions/issues/18)

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { existsSync, lstatSync } = require('fs');
 const { dirname, resolve } = require('path');
 
+const defaultExpectedExtensions = ['js'];
+
+const makeReadableOptions = (options) => {
+    if (options.length === 1) {
+        return options[0];
+    } else if (options.length === 2) {
+        return `${options[0]} or ${options[1]}`;
+    } else {
+        return `one of: ${expectedPattern.join(', ')}`;
+    }
+};
+
 module.exports = {
     configs: {
         recommended: {
@@ -13,28 +25,50 @@ module.exports = {
     },
     rules: {
         'require-extensions': rule((context, node, path) => {
-            if (!existsSync(path)) {
+            const expectedExtensions = context.options[0]?.expectedExtensions ?? defaultExpectedExtensions;
+
+            if (expectedExtensions.length > 0 && !existsSync(path)) {
                 let fix;
                 if (!node.source.value.includes('?')) {
                     fix = (fixer) => {
-                        return fixer.replaceText(node.source, `'${node.source.value}.js'`);
+                        // Trying to find the first existing file with one of the expected extensions
+                        for (const extension of expectedExtensions) {
+                            if (existsSync(`${path}.${extension}`)) {
+                                return fixer.replaceText(node.source, `'${node.source.value}.${extension}'`);
+                            }
+                        }
+
+                        // Otherwise, using the first expected extension
+                        return fixer.replaceText(node.source, `'${node.source.value}.${expectedExtensions[0]}'`);
                     };
                 }
 
+                const expectedPatterns = expectedExtensions.map((ext) => `.${ext}`);
                 context.report({
                     node,
-                    message: 'Relative imports and exports must end with .js',
+                    message: `Relative imports and exports must end with ${makeReadableOptions(expectedPatterns)}`,
                     fix,
                 });
             }
         }),
         'require-index': rule((context, node, path) => {
-            if (existsSync(path) && lstatSync(path).isDirectory()) {
+            const expectedExtensions = context.options[0]?.expectedExtensions ?? defaultExpectedExtensions;
+
+            if (expectedExtensions.length > 0 && existsSync(path) && lstatSync(path).isDirectory()) {
+                const expectedPatterns = expectedExtensions.map((ext) => `index.${ext}`);
                 context.report({
                     node,
-                    message: 'Directory paths must end with index.js',
+                    message: `Directory paths must end with ${makeReadableOptions(expectedPatterns)}`,
                     fix(fixer) {
-                        return fixer.replaceText(node.source, `'${node.source.value}/index.js'`);
+                        // Trying to find the first existing file with one of the expected extensions
+                        for (const extension of expectedExtensions) {
+                            if (existsSync(`${path}/index.${extension}`)) {
+                                return fixer.replaceText(node.source, `'${node.source.value}/index.${extension}'`);
+                            }
+                        }
+
+                        // Otherwise, using the first expected extension
+                        return fixer.replaceText(node.source, `'${node.source.value}/index.${expectedExtensions[0]}'`);
                     },
                 });
             }
@@ -46,13 +80,34 @@ function rule(check) {
     return {
         meta: {
             fixable: true,
+            schema: [
+                {
+                    type: 'object',
+                    properties: {
+                        expectedExtensions: {
+                            type: 'array',
+                            items: {
+                                type: 'string',
+                            },
+                        },
+                    },
+                    additionalProperties: false,
+                },
+            ],
         },
         create(context) {
+            const expectedExtensions = context.options[0]?.expectedExtensions ?? defaultExpectedExtensions;
+
             function rule(node) {
                 const source = node.source;
                 if (!source) return;
                 const value = source.value.replace(/\?.*$/, '');
-                if (!value || !value.startsWith('.') || value.endsWith('.js')) return;
+                if (
+                    !value ||
+                    !value.startsWith('.') ||
+                    expectedExtensions.find((ext) => value.endsWith(`.${ext}`)) !== undefined
+                )
+                    return;
 
                 check(context, node, resolve(dirname(context.getFilename()), value));
             }
